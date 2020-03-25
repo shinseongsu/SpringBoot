2994b09311166fe4baaf4165b2d5018b8e80fbe1
Shin seongsu <tlstjdtn321@naver.com>
2020-03-25 22:21:54 +0900

    Google oauth 사용

---
+++ ch011/src/main/java/com/jojoldu/book/springboot/domain/User/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot.domain.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
